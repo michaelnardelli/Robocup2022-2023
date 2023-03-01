42f6290f3651a7fb6c428253cde1c87712ca6743
michael <michael.nardelli@marconirovereto.it>
2023-03-01 14:40:30 +0100

    varibili

--- progrmmacompleto.ino
+++ progrmmacompleto.ino
@@ -1,5 +1,11 @@
 #include <Servo.h>
 Servo myservo;
+#define Trigavanti 5
+#define Echoavanti 4
+#define Trigsinistra 3
+#define Echosininstra 2
+#define Trigdestra 6
+#define Echodestra 7
 
 #define sensorecolorecentale 10
 #define sensorecoloresinistra 12
@@ -11,6 +17,7 @@ int motori=200;
 double distanzaL=20;
 int angoloa=90;
 int angoloc=25;
+double distanza=0;
 
 //min pin 1
 //max pin 53
@@ -19,8 +26,38 @@ void setup() {
 pinMode(sensorecolorecentale, INPUT);
 pinMode(sensorecoloresinistra, INPUT);
 pinMode(sensorecoloredestra, INPUT);
+pinMode(Trigsinistra,OUTPUT);
+pinMode(Trigavanti,OUTPUT);
+pinMode(Trigsinistra,OUTPUT);
+pinMode(Echodestra,INPUT);
+pinMode(Echosininstra,INPUT);
+pinMode(Echoavanti,INPUT);ù
+}
+int distanzaavanti(){
 
 }
+void sollevamtopallina(){
+
+}
+double calcoladistanzaavanti(){
+    digitalWrite(Trigavanti, LOW);
+delayMicroseconds(2);
+digitalWrite(Trigavanti, HIGH);
+delayMicroseconds(10);
+digitalWrite(Echoavanti, LOW);
+duration = pulseIn(echoPin, HIGH);
+cm = (duration / 2) / 29.1; // la formula per calcolare la distanza in centimetri
+return distatanzavnti;
+} 
 void loop() {
-  if(sensorecolorecentale)
+  if ()
+  {
+    /* code */
+  }
+  
+  
+  
+}
+  
+  
 }
